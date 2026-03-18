786e1c57c0d5fdf5e87f4cdddb0d18f30f707fb3
Remenod <remenod@gmail.com>
2026-03-18 11:07:37 +0200

    make button layout more usual

--- src/UIViews.cpp
+++ src/UIViews.cpp
@@ -7,12 +7,12 @@ using namespace ftxui;
 Component DrawUI(void)
 {
     std::vector<std::vector<std::string>> labels = {
-        {"7", "8", "9", "/", "C"},
-        {"4", "5", "6", "*", "("},
-        {"1", "2", "3", "-", ")"},
-        {"0", ".", "=", "+", "^"},
-        {"sin", "cos", "tan", "log", "ln"},
-        {"pi", "e", "<-", "CE", "Ans"}};
+        {"pi", "!", "(", ")", "%", "C"},
+        {"e", "ln", "7", "8", "9", "/"},
+        {"sin", "log", "4", "5", "6", "*"},
+        {"cos", "sqrt", "1", "2", "3", "-"},
+        {"tan", "^", "0", ".", "=", "+"},
+    };
 
     std::vector<std::vector<Component>> buttons_grid;
     std::vector<Component> rows;
@@ -53,7 +53,7 @@ Component DrawUI(void)
                         buttons_grid[i][j]->Render() | flex | size(WIDTH, GREATER_THAN, 10) | border | center);
                 }
 
-                row_elements.push_back(hbox(std::move(button_elements)));
+                row_elements.push_back(hbox(std::move(button_elements) | flex) | flex);
             }
 
             return vbox(std::move(row_elements));
